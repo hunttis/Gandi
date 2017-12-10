11d5104d67198eaba28f0b15baee4e00ba948e47
Ville Nuutinen <hunttis@me.com>
2017-12-10 10:12:19 +0200

    Removed usesess code

--- src/Server.hx
+++ src/Server.hx
@@ -12,19 +12,14 @@ class Server {
     app.use(new Favicon(Node.__dirname + '/public/favicon.ico'));
     app.set('port', untyped(js.Node.process.env.PORT || 3000) );
     
-    // there is no Logger class in js-kit, so I added it in this source folder (js/npm/express/Logger.hx)
-    // app.use(new Logger('dev')); 
     app.use(BodyParser.urlencoded());
-    // app.use(new MethodOverride()); // can't find it in js-kit AND don't know what it does...
+    
     app.use(new Static(Path.join(Node.__dirname, 'public')));
 
-    // Routes
-    // http://localhost:3000
     app.get('/', function (req, res) {
         res.sendfile(Node.__dirname + '/public/index.html');
     });
 
-    // http://localhost:3000/nope
     app.use(function(req, res, next) {
         res.status(404).send('404');
     });
